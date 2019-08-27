89b7714afa91fbf5e2813a067af9f911b313ce4f
good-singer <15732658216@163.com>
2019-08-27 19:53:23 +0800

    实现搜索功能

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Navbar = ({ icon, title }) => {
+  // 设置默认属性值
+
+
+
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  )
+}
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fa fa-github"
+}
+
+// 定义属性类型
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+}
+
+export default Navbar;
