b57bb5b6f309b2846fb2037e0972d471305c4c32
good-singer <15732658216@163.com>
2019-08-28 08:35:39 +0800

    实现Search请求接口和Clear数据

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 
 export default class App extends Component {
@@ -9,22 +10,41 @@ export default class App extends Component {
     users: [],
     loading: false
   }
-  async componentDidMount() {
+
+  // async componentDidMount() {
+  //   this.setState({ loading: true })
+  //   const res = await axios.get(`https://api.github.com/user?
+  //     client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+  //     client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false })
+  // }
+
+  searchUsers = async text => {
     this.setState({ loading: true })
-    const res = await axios.get(`https://api.github.com/users?
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&
       client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
       client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false })
+    this.setState({ users: res.data.items, loading: false })
   }
+
+  clearUsers = () => this.setState({ users: [], loading: false })
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search
+            showClear={users.length > 0 ? true : false}
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+          />
           <Users
-            loading={this.state.loading}
-            users={this.state.users}
+            loading={loading}
+            users={users}
           />
         </div>
 
