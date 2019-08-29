399e2f7590ba0c3726cd1bb64de66840e32b8fa2
good-singer <15732658216@163.com>
2019-08-29 14:46:16 +0800

    打包

---
+++ build/precache-manifest.f7341b59c1675b8820360b3fbfeb4ff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14c1b0acd16adf3c5844524fb8b6750",
+    "url": "/react-github-finder/index.html"
+  },
+  {
+    "revision": "6b5f83f735a33b1e525a",
+    "url": "/react-github-finder/static/css/main.b4f77495.chunk.css"
+  },
+  {
+    "revision": "f34febc0f4511fa10344",
+    "url": "/react-github-finder/static/js/2.b4f3d42c.chunk.js"
+  },
+  {
+    "revision": "6b5f83f735a33b1e525a",
+    "url": "/react-github-finder/static/js/main.6b4a9a03.chunk.js"
+  },
+  {
+    "revision": "74bf82ac669e81697831",
+    "url": "/react-github-finder/static/js/runtime~main.9c99d210.js"
+  }
+]);
